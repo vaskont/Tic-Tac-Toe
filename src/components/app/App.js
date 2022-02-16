55c988f95d4adfb650ac173df379534faa66bec5
Vassilis Kontopoulos <bkontopoulos96@gmail.com>
2022-02-16 13:57:30 +0200

    app restored

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,3 +10,20 @@ import { Provider } from 'libs/model';
 
 import './app.css';
 
+function App() {
+
+    const [state, dispatch] = useReducer(reducer, reducerInitState);
+
+    return (
+        <Provider 
+            value={{
+                state,
+                dispatch,
+            }}
+        >
+            <Game />
+        </Provider>
+    );
+  }
+
+export default App;
