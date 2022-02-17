eb5acf9ddecfe5d13266681331bb532dc968aee2
Vassilis Kontopoulos <bkontopoulos96@gmail.com>
2022-02-17 13:16:50 +0200

    redux v3

---
+++ src/redux/store.js
@@ -0,0 +1,22 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import { 
+  reducer, 
+  reducerInitState,
+} from 'models/tic-tac-toe';
+
+function configureStore(preloadedState) {
+  const middlewares = []
+  const middlewareEnhancer = applyMiddleware(...middlewares)
+
+  const enhancers = [middlewareEnhancer]
+  const composedEnhancers = composeWithDevTools(...enhancers)
+
+  const store = createStore(reducer, preloadedState, composedEnhancers)
+
+  return store
+}
+
+const store = configureStore(reducerInitState);
+export default store;
